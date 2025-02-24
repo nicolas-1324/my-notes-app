05c2dbc2bc759547db521e96d422ba85e8f16f01
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-24 19:42:50 +0100

    napredek

--- MyNotes.java
+++ MyNotes.java
@@ -10,17 +10,25 @@ public class MyNotes{
 		BufferedReader vhodText = new BufferedReader(new InputStreamReader(System.in));
 		Scanner in = new Scanner(System.in);
 
-		System.out.print("Do you want to make a new note or write to an existing one [new/write]:");
-		//Note note = new Note();	
+		System.out.print("Do you want to make a new note or read/write to an existing one [new/read/write]:");
 		String izbira = vhodText.readLine();
 		int st_note;
 		switch(izbira){
 			case "new":
-				note = vhodText.readLine();
-				
+				System.out.println("Vnesi niz/note:");
+				String vnesenNiz = vhodText.readLine();
+
+				System.out.println("Vnesi mesto na katerega ga hoces shranit:");
+				st_note = in.nextInt();
+				Notes note = new Notes(vnesenNiz, st_note);
 				break;
-			case "write":
-				System.out.println("To which file do you want to write");
+			case "edit":
+				System.out.println("Which file do you want to edit");
+				break;
+			case "read":
+				System.out.println("Which file do you want to read");
+				st_note = in.nextInt();
+				note.izpis(st_note);
 				break;
 			default:
 				System.out.println("Vnesel veljaven vnos!");
--- Notes.java
+++ Notes.java
@@ -1,17 +1,11 @@
-public class Note{
+public class Notes{
 	private String note;
 	private int st_note;
 	private String[] storage = new String[10];
 
-	public Note(){
-		this.note = note;
-	}
+	public Notes(){
 
-	public void newNote(Note(), int st_note){
-		this.Note = Note;
-		this.st_note = st_note;
-		this.storage[st_note] = Note;
-	}
+	} 
 
 	public String izpis(int st_note){
 		return storage[st_note];
